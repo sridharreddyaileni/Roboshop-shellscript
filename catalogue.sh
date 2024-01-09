8de899add044aaf29a8e807ccb4c1327cefcd686
sridharreddyaileni <sridharreddy2508@gmail.com>
2024-01-09 16:37:59 +0530

    Roboshop shell script updated

---
+++ catalogue.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started and executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILED $N"
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+else
+    echo -e "$G You are the root user $N"
+fi
+
+dnf module disable nodejs -y &>> $LOGFILE
+
+VALIDATE $? "Disabling current Nodejs" 
+
+dnf module enable nodejs:18 -y &>> $LOGFILE
+
+VALIDATE $? "Enabling current Nodejs:18" 
+
+dnf install nodejs -y &>> $LOGFILE
+
+VALIDATE $? "Installing Nodejs:18" 
+
+useradd roboshop &>> $LOGFILE
+
+VALIDATE $? "creating roboshop user" 
+
+mkdir /app &>> $LOGFILE
+
+VALIDATE $? "creating app directory" 
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
+
+VALIDATE $? "Downloading catalogue application" 
+
+cd /app &>> $LOGFILE
+
+unzip /tmp/catalogue.zip &>> $LOGFILE
+
+VALIDATE $? "unzipping catalogue" 
+
+npm install &>> $LOGFILE
+
+VALIDATE $? "installing dependencies" 
+
+cp catalogue.service /etc/systemd/system/catalogue.service &>> $LOGFILE
+
+VALIDATE $? "copying catalogue service file" &>> $LOGFILE
+
+systemctl daemon-reload &>> $LOGFILE
+
+VALIDATE $? "Catalogue daemon reload" 
+
+systemctl enable catalogue &>> $LOGFILE
+
+VALIDATE $? "Enabling Catalogue" 
+
+systemctl start catalogue &>> $LOGFILE
+
+VALIDATE $? "starting Catalogue" 
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE 
+
+VALIDATE $? "copying mongodb repo"
+
+dnf install mongodb-org -y &>> $LOGFILE
+
+VALIDATE $? "Installing mongodb client"
+
+mongo --host $MONGODB_HOST </app/schema/catalogue.js
+
+VALIDATE $? "Loading catalogue data into mongodb"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
