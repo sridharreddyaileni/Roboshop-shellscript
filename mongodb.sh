6f9aeeb1056eafca763f285fda3a4f27b0b835a1
sridharreddyaileni <sridharreddy2508@gmail.com>
2024-01-09 13:54:32 +0530

    Roboshop shell script updated

--- mongodb.sh
+++ mongodb.sh
@@ -30,7 +30,7 @@ else
     echo -e "$G You are the root user $N"
 fi
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+cp mongodb.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
 VALIDATE $? "copied mongodb repo"
 
