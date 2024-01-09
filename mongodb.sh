c87d846c328dd85471b92bbde005e6986230a2aa
sridharreddyaileni <sridharreddy2508@gmail.com>
2024-01-09 13:09:54 +0530

    Roboshop shell script updated

--- mongodb.sh
+++ mongodb.sh
@@ -33,3 +33,24 @@ fi
 cp mongodb.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
 VALIDATE $? "copied mongodb repo"
+
+dnf install mongodb-org -y &>> $LOGFILE
+
+VALIDATE $? "Installing mongodb"
+
+systemctl enable mongod
+
+VALIDATE $? "Enabling mongodb"
+
+systemctl start mongod
+
+VALIDATE $? "Starting mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+
+VALIDATE $? "remote access to mongodb"
+
+systemctl restart mongod &>> $LOGFILE
+
+VALIDATE $? "restarting mongodb"
+
